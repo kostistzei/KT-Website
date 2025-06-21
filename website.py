2c856251609b237f917b2b3fe4c9c3a9cd5ff0b4
Konstantinos Tzeiranidis <kotsios462@gmail.com>
2025-06-21 23:05:34 +0300

    fix: set host and port for Render

--- website.py
+++ website.py
@@ -33,4 +33,4 @@ def download_file(filename):
 if __name__ == "__main__":
     import sys
     if not hasattr(sys, 'ps1'):
-        app.run(debug=True, use_reloader=False)
+        app.run(host="0.0.0.0", port=10000)
